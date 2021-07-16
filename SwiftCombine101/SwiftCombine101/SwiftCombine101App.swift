9abcdf5a8aec0fa54439da61d6816f802e55a0bc
Marijan <marijanv@yahoo.com>
2021-07-16 08:47:52 -0700

    first commit

---
+++ SwiftCombine101/SwiftCombine101/SwiftCombine101App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCombine101App.swift
+//  SwiftCombine101
+//
+//  Created by MARIJAN VUKCEVICH on 7/16/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCombine101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
